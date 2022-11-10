d846352a4c5dcb98699c748f7c291995b57d143f
baracuda <israelcantu129@gmail.com>
2022-11-10 09:59:37 -0600

    Rename class and file to match Kattis' files

--- src/echoechoecho.java
+++ src/echoechoecho.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Echo {
+public class echoechoecho {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         String word = scan.next();
