6004e2c03d5a95c7b0d0b21c1696959d29d84d04
BuildTools <unconfigured@null.spigotmc.org>
2022-11-09 21:44:57 -0600

    hi

---
+++ src/Timeloop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Timeloop {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int counter = scan.nextInt();
+        
+        for (int i = 1; i <= counter; i++)
+            System.out.println(i + " Abracadabra");
+
+    }
+}
---
+++ src/WhichIsGreater.java
@@ -0,0 +1,12 @@
+public class WhichIsGreater {
+    public static void main(String[] args) {
+        System.out.println(WhichIsGreater(13, 14));
+    }
+    public static int WhichIsGreater(int num1, int num2) {
+        
+        if (num1 > num2) 
+            return 1;
+        else 
+            return 0;
+    }
+}
