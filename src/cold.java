adeeec1bdd52ee1f2c5e7a2675705662cd438295
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 22:50:34 -0600

    Solved Cold-puter Science

---
+++ src/cold.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class cold {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int counter = 0;
+        for (int i = 0; i < n; i++) {
+            if (scan.nextInt() < 0) 
+                counter++;
+        }
+        System.out.println(counter);
+    }
+}
