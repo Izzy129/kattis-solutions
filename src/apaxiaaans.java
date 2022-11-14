755d52e5dc08a4efcb02f02fef4e0b8006dabeee
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 22:44:19 -0600

    Solved Apaxiaaaaaaaaaaaans!

---
+++ src/apaxiaaans.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class apaxiaaans {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        String[] sArray = s.split("");
+        System.out.print(sArray[0]);
+        for (int i = 1; i < sArray.length; i++) {
+            if (sArray[i].equalsIgnoreCase(sArray[i-1])) {
+            } else {
+                System.out.print(sArray[i]);
+            }
+        }
+    }
+}
