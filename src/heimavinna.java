45317cd297672e6bcd3d91a5cd5f652ca293e073
baracuda <israelcantu129@gmail.com>
2022-11-18 13:06:07 -0600

    Solved Homework
    This solution is kind of inefficient
    I created another solution that processed in half the time and worked on my IDE, although not all of the test cases were correct on Kattis' judging. 
    Here's the more efficient ~solution: https://pastebin.com/raw/eNhCVFWJ

---
+++ src/heimavinna.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+import java.util.ArrayList;
+
+public class heimavinna {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        scan.useDelimiter(";"); // to separate problem sets
+        
+        int totalProblems = 0;
+        
+        while (scan.hasNext()) {
+
+            String current = scan.next();
+            current = current.replaceAll("-", " ");
+            
+            Scanner currentScan = new Scanner(current);
+            ArrayList<Integer> theList = new ArrayList<>();
+            
+            while (currentScan.hasNext()) 
+                theList.add(currentScan.nextInt());
+            if (theList.size() == 1) {
+                totalProblems++;
+            } else {
+                totalProblems+= theList.get(1) - theList.get(0) + 1;
+            }
+        }
+        System.out.println(totalProblems);
+    }
+}
