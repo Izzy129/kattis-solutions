2d014f8f971e66c3b6f2da8b8504d2af693b4e90
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 23:02:00 -0600

    Solved Greetings!

---
+++ src/greetings2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class greetings2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int numOfEs = (s.length() - 2) * 2;
+        System.out.print("h");
+        for (int i = 0; i < numOfEs; i++) {
+            System.out.print("e");
+        }
+        System.out.print("y");
+    }
+}
