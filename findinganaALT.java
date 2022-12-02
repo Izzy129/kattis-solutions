2a5806a078840b4b2f37b079b58807f3c2abaacb
baracuda <israelcantu129@gmail.com>
2022-12-02 13:57:43 -0600

    Solved Finding an A (alt solution)
    I solved this problem with BufferedWriter/BufferedReader instead of using Scanner to experiment with it
    It was 3 ms (0.06s) faster than using Scanner (0.09s)
    
    Usual Scanner solution can be found [here](https://github.com/Izzy129/kattis-solutions/blob/master/src/findingana.java)

---
+++ findinganaALT.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class findingana {
+    public static void main(String args[]) throws IOException {
+        BufferedReader reader = null;
+        BufferedWriter output = null;
+        try {
+            String s;
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            output = new BufferedWriter(new OutputStreamWriter(System.out));
+            
+            s = reader.readLine();
+            reader.close();
+            
+            output = new BufferedWriter(new OutputStreamWriter(System.out));
+            output.write(s.substring(s.indexOf("a")));
+            output.close();
+        } finally { }
+    }
+}
