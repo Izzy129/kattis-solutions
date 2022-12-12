5e968d5111b9db6a3d8822749651fdf29925ec87
baracuda <israelcantu129@gmail.com>
2022-12-12 16:03:30 -0600

    Solved Sort Two Numbers

---
+++ src/sorttwonumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class sorttwonumbers {
+    public static void main(String args[]) {
+      Scanner scan = new Scanner(System.in);
+      int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
+      
+      if (num2 > num1) {
+          System.out.println(num1 + " " + num2);
+      } else {
+          System.out.println(num2 + " " + num1);
+      }
+    }
+}
